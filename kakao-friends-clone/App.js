1c82e0548f25fba688ab24c656c416ccb5963dc6
sujeong-DN <sjp@digitalnutrition.info>
2023-05-31 13:20:18 +0900

    [Add] MyProfile

--- kakao-friends-clone/App.js
+++ kakao-friends-clone/App.js
@@ -1,18 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, Platform } from 'react-native';
+import { StyleSheet, Text, View, Platform } from 'react-native';
 import Header from './src/Header.js';
-
 import { getStatusBarHeight, getBottomSpace } from 'react-native-iphone-x-helper';
+import MyProfile from './src/MyProfile.js';
+import {myProfile} from './src/data.js';
+import Margin from './src/Margin.js';
 
 const statusBarHeight = getStatusBarHeight(true);
 const bottomSpace = getBottomSpace();
-
-console.log(`${Platform.OS} : ${statusBarHeight}, ${bottomSpace}`)
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Header />
+
+      <Margin height={10} />
+
+      <MyProfile 
+        uri={myProfile.uri}
+        name={myProfile.name}
+        introduction={myProfile.introduction}
+      />
     </View>
   );
 }
@@ -23,6 +31,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     paddingTop: statusBarHeight,
     paddingBottom: bottomSpace,
-    // justifyContent: "flex-end",
   },
 });
