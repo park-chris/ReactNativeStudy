f8ba8585a1710193413c46686862cb993b9cd5e3
sujeong-DN <sjp@digitalnutrition.info>
2023-06-01 14:06:23 +0900

    [Refactor] Calculator.js에서 뷰와 로직 분리

--- calculator/src/Calculator.js
+++ calculator/src/Calculator.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, TouchableOpacity, Text } from 'react-native';
 import styled from 'styled-components';
-import { useState } from 'react';
+import { useCalculator } from './use-calculator';
 
 
 // Button type: 'reset' | 'operator' | 'num'
@@ -55,86 +55,32 @@ padding: 1px 2px 3px 4px; // top right bototm left */
 `;
 
 export default () => {
-    const [input, setInput] = useState(0);
-    const [currentOperator, setCurrentOperator] = useState(null);
-    const [result, setResult] = useState(null);
-    const [tempInput, setTempInput] = useState(null);
-    const [tempOperator, setTempOperator] = useState(null);
-    const [isClickedOperator, setIsClickedOperator] = useState(false);
-    const [isClickedEqual, setIsClickedEqual] = useState(false);
 
-    // 어떤 값을 Boolean값으로 변화할때, 아래와 같은 식임
-    // const hasInput = input ? true : false ;
-    const hasInput = !!input;
-
-
-    const onPressNum = (num) => {
-
-        if (currentOperator && isClickedOperator) {
-            setResult(input);
-            setInput(num);
-            setIsClickedOperator(false);
-        } else {
-            const newInput = Number(`${input}${num}`)
-            setInput(newInput);
-        }
-    }
-
-    const onPressOperator = (operator) => {
-        if (operator !== "=") {
-            setCurrentOperator(operator);
-            setIsClickedOperator(true);
-            setIsClickedEqual(false);
-        } else {
-            let finalResult = result;
-            const finalInput = isClickedEqual ? tempInput : input;
-            const finalOperator = isClickedEqual ? tempOperator : currentOperator;
-            switch (finalOperator) {
-                case '+':
-                    finalResult = result + finalInput;
-                    break;
-                case '-':
-                    finalResult = result - finalInput;
-                    break;
-                case '*':
-                    finalResult = result * finalInput;
-                    break;
-                case '/':
-                    finalResult = result / finalInput;
-                    break;
-                default:
-                    break;
-            }
-            setResult(finalResult);
-            setInput(finalResult);
-            setTempInput(finalInput)
-            setCurrentOperator(null);
-            setTempOperator(finalOperator)
-            setIsClickedEqual(true);
-
-        }
-    }
-
-    const onPressReset = () => {
-        if (hasInput) {
-            setInput(0);
-        } else {
-            setInput(0);
-            setCurrentOperator(null);
-            setResult(null);
-            setTempInput(null);
-            setTempOperator(null);
-        }
-    }
+    const {
+        input,
+        currentOperator,
+        result,
+        tempInput,
+        tempOperator,
+        hasInput,
+        onPressNum,
+        onPressOperator,
+        onPressReset,
+    } = useCalculator();
 
     return (
         <View style={{ flex: 1, width: 250, justifyContent: "center" }}>
 
-            <Text>input: {input}</Text>
-            <Text>currentOperator: {currentOperator}</Text>
-            <Text>result: {result}</Text>
-            <Text>tempInput: {tempInput}</Text>
-            <Text>tempOperator: {tempOperator}</Text>
+            {/* 개발 환경에서만 보이게하는 로직 */}
+            {__DEV__ && (
+                <>
+                    <Text>input: {input}</Text>
+                    <Text>currentOperator: {currentOperator}</Text>
+                    <Text>result: {result}</Text>
+                    <Text>tempInput: {tempInput}</Text>
+                    <Text>tempOperator: {tempOperator}</Text>
+                </>
+            )}
 
             {/* 결과 */}
             <Inputontainer>
