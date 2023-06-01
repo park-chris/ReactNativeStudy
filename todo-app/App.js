4e3a3d47bcb9ba7cda4bfd0ab0683f1e9955d402
sujeong-DN <sjp@digitalnutrition.info>
2023-06-01 16:23:13 +0900

    [Add] 날짜 선택 이벤트 추가

--- todo-app/App.js
+++ todo-app/App.js
@@ -1,27 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { FlatList, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { FlatList, SafeAreaView, StyleSheet, Text, TouchableOpacity, View,StatusBar } from 'react-native';
 import { getCalendarColumns, getDayText, getDayColor } from './src/utils';
 import dayjs from 'dayjs';
 import { runPracticeDayjs } from './src/practice-dayjs';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Margin from './src/Margin';
 import { SimpleLineIcons } from '@expo/vector-icons';
+
+const marginTop = Platform.OS === "android" ? StatusBar.currentHeight : 0
 
 const columnSize = 35;
 
 const Column = ({
   text,
   color,
-  opacity
+  opacity,
+  disabled,
+  onPress,
+  isSelected,
 }) => {
   return (
-    <View style={{ width: columnSize, height: columnSize, justifyContent: "center", alignItems: "center" }}>
+    <TouchableOpacity
+      disabled={disabled}
+      onPress={onPress}
+      style={{
+        width: columnSize,
+        height: columnSize,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: isSelected ? "#c2c2c2" : "transparent",
+        borderRadius: columnSize / 2,
+      }}>
       <Text style={{ color, opacity }}>{text}</Text>
-    </View>
+    </TouchableOpacity>
   )
 }
 
-const ArrowButton = ({iconName, onPress}) => {
+const ArrowButton = ({ iconName, onPress }) => {
   return (
     <TouchableOpacity onPress={onPress} style={{ paddingHorizontal: 20, paddingVertical: 15 }}>
       <SimpleLineIcons name={iconName} size={15} color="#404040" />
@@ -32,16 +46,19 @@ const ArrowButton = ({iconName, onPress}) => {
 export default function App() {
 
   const now = dayjs();
-  const columns = getCalendarColumns(now);
+  const [selectedDate, setSelectedDate] = useState(now);
+
+  const columns = getCalendarColumns(selectedDate);
+
   const ListHeaderComponent = () => {
-    const currentDateText = dayjs(now).format("YYYY.MM.DD.");
+    const currentDateText = dayjs(selectedDate).format("YYYY.MM.DD.");
     return (
       <View>
         {/* <YYYY.MM.DD> */}
         <View style={{ flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
-          <ArrowButton iconName={"arrow-left"} onPress={() => {}}/>
+          <ArrowButton iconName={"arrow-left"} onPress={() => { }} />
           <Text style={{ fontSize: 20, color: "#404040" }}>{currentDateText}</Text>
-          <ArrowButton iconName={"arrow-right"} onPress={() => {}}/>
+          <ArrowButton iconName={"arrow-right"} onPress={() => { }} />
         </View>
 
         <View style={{ flexDirection: 'row' }}>
@@ -49,11 +66,17 @@ export default function App() {
             const dayText = getDayText(day);
             const color = getDayColor(day);
             return (
-              <Column key={`day-${day}`} text={dayText} color={color} opacity={1} />
+              <Column
+                key={`day-${day}`}
+                text={dayText}
+                color={color}
+                opacity={1}
+                disabled={true}
+              />
             )
           })}
         </View>
-        
+
       </View>
     )
   }
@@ -62,9 +85,19 @@ export default function App() {
     const dateText = dayjs(date).get('date');
     const day = dayjs(date).get('day');
     const color = getDayColor(day);
-    const isCurrentMonth = dayjs(date).isSame(now, 'month');
+    const isCurrentMonth = dayjs(date).isSame(selectedDate, 'month');
+    const onPress = () => {
+      setSelectedDate(date);
+    }
+    const isSelected = dayjs(date).isSame(selectedDate, 'date');
     return (
-      <Column text={dateText} color={color} opacity={isCurrentMonth ? 1 : 0.4} />
+      <Column 
+      text={dateText} 
+      color={color} 
+      opacity={isCurrentMonth ? 1 : 0.4}
+      onPress={onPress}
+      isSelected={isSelected}
+     />
     )
   }
 
@@ -73,10 +106,14 @@ export default function App() {
     console.log('columns', columns);
   }, []);
 
+  useEffect(() => {
+      console.log('changed selectedDate', dayjs(selectedDate).format("YYYY.MM.DD"));
+  }, [selectedDate]);
+
   return (
     <SafeAreaView style={styles.container}>
       <FlatList
-      keyExtractor={(_, index) => `column-${index}`}
+        keyExtractor={(_, index) => `column-${index}`}
         data={columns}
         renderItem={renderItem}
         numColumns={7}
@@ -92,5 +129,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: marginTop,
   },
 });
